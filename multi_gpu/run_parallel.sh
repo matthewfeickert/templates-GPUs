d47610116f88812479f94ff6d7221479be4e9f98
Matthew Feickert <matthew.feickert@cern.ch>
2025-11-03 23:52:29 -0700

    feat: Convert to Pixi

--- multi_gpu/run_parallel.sh
+++ multi_gpu/run_parallel.sh
@@ -1,24 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e
+# detailed logging to stderr
+set -x
 
 NUMGPUS=$1
 
-echo "Number of GPUs requested: $NUMGPUS"
-echo "GPUs assigned: $CUDA_VISIBLE_DEVICES"
+echo "Number of GPUs requested: ${NUMGPUS}"
+echo "GPUs assigned: ${CUDA_VISIBLE_DEVICES}"
 
-# installation steps for Miniconda
-export HOME=$PWD
-sh Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
-rm Miniconda3-latest-Linux-x86_64.sh
-export PATH=$HOME/miniconda3/bin:$PATH
-source $HOME/miniconda3/etc/profile.d/conda.sh
-hash -r
-conda config --set always_yes yes --set changeps1 no
+echo -e "\n# Check to see if the NVIDIA drivers can correctly detect the GPU:\n"
+nvidia-smi
 
-# install environment
-conda env create -f env.yml -n multigpu
-conda activate multigpu
+echo -e "# Installing Pixi"
+curl -fsSL https://pixi.sh/install.sh | bash
+. ~/.bashrc
 
 #wandb login <your api key>
 
-python3 model_parallel.py -n $NUMGPUS
+pixi run python model_parallel.py -n "${NUMGPUS}"
